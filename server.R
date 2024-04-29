66b73000afc63ecb848f40335540be42dabfa0e8
Sudhir Voleti <sudhir_voleti@isb.edu>
2024-04-29 18:50:11 +0530

    Update server.R

--- server.R
+++ server.R
@@ -420,8 +420,12 @@ lexicon_data<-read.csv('sentiments.csv',stringsAsFactors=FALSE)# read lexcicons 
     if (is.null(input$file)|input$apply==0) {return(NULL)} 
     else {
       tb = sentiments.index()
-      textdf = dataset1()[input$index,1]
-      tx = textdf |> unnest_tokens(text, text, token = "sentences")      
+      #textdf = dataset1()
+      tx = data.frame(Doc.id = 1:nrow(textdf), text=dataset1()) |>                 
+                unnest_tokens(text, text, token = "sentences") |>
+                mutate(Sentence.No. = row_number()) |> filter(Doc.id == input$index)                
+      
+      #tx = textdf |> unnest_tokens(text, text, token = "sentences")      
       y1 = data.frame(tx, Sentence.No = 1:nrow(tx))    
       test = merge(tb, y1, by.x ="Sentence.No", by.y= "Sentence.No", all.y=T)
      return(test)
